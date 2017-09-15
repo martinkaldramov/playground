bfb415b26f9510a702aed1bc17d2e09159e4546a
Marto <martinkaldramov1@gmail.com>
2017-09-15 07:28:35 +0300

    using object destructuring to pull out mongoclient and objectid from mongodb module

--- mongodb-connect.js
+++ mongodb-connect.js
@@ -1,4 +1,7 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   if(err)
@@ -16,16 +19,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //     console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Martin Kaldramov',
-    age: 25,
-    location: 'Sofia, Bulgaria'
-  }, (err, result) => {
-    if(err)
-      return console.log('Error adding a user');
+  // db.collection('Users').insertOne({
+  //   name: 'Martin Kaldramov',
+  //   age: 25,
+  //   location: 'Sofia, Bulgaria'
+  // }, (err, result) => {
+  //   if(err)
+  //     return console.log('Error adding a user');
 
-    console.log(JSON.stringify(result.ops, undefined, 2));  
-  });
+  //   console.log(JSON.stringify(result.ops, undefined, 2));  
+  // });
 
   db.close();
 });
