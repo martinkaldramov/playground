fae68faedf6ee9a228110a18ddd7029094c2ce48
Marto <martinkaldramov1@gmail.com>
2017-09-15 07:02:24 +0300

    successfully added a todo item to the collection

--- mongodb-connect.js
+++ mongodb-connect.js
@@ -6,5 +6,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   console.log('Connected to MongoDB server successfully');
 
+  db.collection('Todos').insertOne({
+    text: 'A todo item #1',
+    completed: false
+  }, (err, result) => {
+    if(err)
+       return console.log('Error adding a file to the Todos collection');
+
+      console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
   db.close();
 });
