1e6738324c82165d91b6ed73cb5f3c123ff356b3
Marto <martinkaldramov1@gmail.com>
2017-09-15 07:08:56 +0300

    added a user to the Users collection

--- mongodb-connect.js
+++ mongodb-connect.js
@@ -6,14 +6,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   console.log('Connected to MongoDB server successfully');
 
-  db.collection('Todos').insertOne({
-    text: 'A todo item #1',
-    completed: false
+  // db.collection('Todos').insertOne({
+  //   text: 'A todo item #1',
+  //   completed: false
+  // }, (err, result) => {
+  //   if(err)
+  //      return console.log('Error adding a file to the Todos collection');
+
+  //     console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Martin Kaldramov',
+    age: 25,
+    location: 'Sofia, Bulgaria'
   }, (err, result) => {
     if(err)
-       return console.log('Error adding a file to the Todos collection');
+      return console.log('Error adding a user');
 
-      console.log(JSON.stringify(result.ops, undefined, 2));
+    console.log(JSON.stringify(result.ops, undefined, 2));  
   });
 
   db.close();
