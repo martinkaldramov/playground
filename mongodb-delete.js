04e5f0f60ff6a8a36aba22f2fc78280178ee672a
Marto <martinkaldramov1@gmail.com>
2017-09-17 15:47:27 +0300

    deleting records in MongoDB with different methods and a name query

--- mongodb-delete.js
+++ mongodb-delete.js
@@ -9,9 +9,33 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   console.log('Connected to MongoDB server successfully');
 
-  db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
-    console.log(result);
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+  
+  db.collection('Users').deleteMany({name: 'Martin Kaldramov'}).then((res) => {
+    console.log(res);
+  }, (err) => {
+    console.log('Unable to delete the entries', err);
   });
+
+  // db.collection('Users').findOneAndDelete({name: 'Somebody Else'}).then((res) => {
+  //   console.log('Deleting entry with name: Somebody Else successful');
+  //   console.log(res);
+  // }, (err) => {
+  //   console.log('Unable to delete the entry with name: Somebody Else', err);
+  // });
 
   // db.close();
   
