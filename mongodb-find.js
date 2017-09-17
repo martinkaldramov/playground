12b2e2d80b95640b52cf5553bed7c82dce9a86b1
Marto <martinkaldramov1@gmail.com>
2017-09-17 12:46:35 +0300

    counting all the documents in the collection with mongo's native driver for node-js

--- mongodb-find.js
+++ mongodb-find.js
@@ -9,12 +9,19 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   console.log('Connected to MongoDB server successfully');
 
-  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
+  // db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch the data', err);
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
   }, (err) => {
     console.log('Unable to fetch the data', err);
   });
+
 
   // db.close();
   
