6cb74b8db158d8d1f5d277f228599cc8a7331e68
Marto <martinkaldramov1@gmail.com>
2017-09-17 13:17:56 +0300

    Fetching users from the database

--- mongodb-find.js
+++ mongodb-find.js
@@ -16,8 +16,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log('Unable to fetch the data', err);
   // });
 
-  db.collection('Todos').find().count().then((count) => {
-    console.log(`Todos count: ${count}`);
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch the data', err);
+  // });
+
+  db.collection('Users').find({name: 'Martin Kaldramov'}).toArray().then((docs) => {
+    console.log(`Users with name Martin Kaldramov:`);
+    console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
     console.log('Unable to fetch the data', err);
   });
